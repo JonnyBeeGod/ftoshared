0c7c44fc1a0301e6744d3dcd41248c39bfae2607
Jonas Reichert <jonas.reichert@mailbox.org>
2021-04-03 16:39:48 +0200

    Revert "[UPDATE] Export.swift"
    This reverts commit 62f105301029f8bde4b41c049680440f9e68cbd2.

--- Sources/Ftoshared/Export.swift
+++ Sources/Ftoshared/Export.swift
@@ -7,25 +7,15 @@ import Foundation
 public struct Export: Codable, Equatable {
     
     public let templates: [TemplateAPIModel]
-    public let weeks: [TemplateWeekAPIModel]
-    public let workouts: [TemplateWorkoutAPIModel]
-    public let exercises: [TemplateExerciseAPIModel]
-    public let sets: [TemplateExerciseSetAPIModel]
     public let types: [TemplateTypeAPIModel]
     public let lifts: [LiftAPIModel]
     
-    public init(templates: [TemplateAPIModel],
-                weeks: [TemplateWeekAPIModel],
-                workouts: [TemplateWorkoutAPIModel],
-                exercises: [TemplateExerciseAPIModel],
-                sets: [TemplateExerciseSetAPIModel],
-                types: [TemplateTypeAPIModel],
-                lifts: [LiftAPIModel]) {
+    public init(
+        templates: [TemplateAPIModel],
+        types: [TemplateTypeAPIModel],
+        lifts: [LiftAPIModel]) {
+        
         self.templates = templates
-        self.weeks = weeks
-        self.workouts = workouts
-        self.exercises = exercises
-        self.sets = sets
         self.types = types
         self.lifts = lifts
     }
